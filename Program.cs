2c9604bfdba173c2c7077a3d4c8556da5ac2a657
Piwowarski Wojciech <PiwowarskiW@stat.intra>
2025-12-19 14:03:43 +0100

    init test2

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Console_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
